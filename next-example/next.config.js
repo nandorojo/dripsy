0aeb6e3db34236ae59a5d3acec474f7e0a118039
Fernando Rojo <frojo@sas.upenn.edu>
2020-08-25 12:48:31 -0400

    Update next.config.js

--- next-example/next.config.js
+++ next-example/next.config.js
@@ -2,7 +2,7 @@
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')([
-  '@nandorojo/dripsy',
+  'dripsy',
   // 'styled-components',
   // 'styled-components/native',
 ]);
