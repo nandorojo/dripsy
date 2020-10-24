71851b5b985b68666f52903efb8e409034237f30
Chadwick Maycumber <chad.maycumber11@gmail.com>
2020-10-23 20:24:35 -0400

    Updated the master branch with most recent changes for yarn workspaces

--- examples/example/web-build/register-service-worker.js
+++ examples/example/web-build/register-service-worker.js
@@ -1,13 +1,13 @@
 /* eslint-env browser */
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
+  window.addEventListener('load', function() {
     navigator.serviceWorker
       .register('/expo-service-worker.js', { scope: '/' })
-      .then(function (info) {
+      .then(function(info) {
         // console.info('Registered service-worker', info);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.info('Failed to register service-worker', error);
       });
   });
---
+++ examples/next-example/next.config.js
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/no-var-requires */
+// @generated: @expo/next-adapter@2.1.0
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
+const withPlugins = require('next-compose-plugins')
+// const withTM = require('next-transpile-modules')(['dripsy'])
+const { withExpo } = require('@expo/next-adapter')
+
+const path = require('path')
+const fs = require('fs')
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+
+const node_modules = path.resolve(__dirname, '../..', 'node_modules')
+const packages = path.resolve(__dirname, '../..', 'packages')
+
+module.exports = withPlugins([
+	withExpo({
+		projectRoot: __dirname,
+		webpack: async (config) => {
+			config.module.rules.push({
+				test: /\.(js|ts|tsx)$/,
+				include: /(packages|next-example)\/.+/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			})
+
+			config.resolve.plugins = config.resolve.plugins.filter(
+				(p) => !(p instanceof ModuleScopePlugin)
+			)
+
+			Object.assign(config.resolve.alias, {
+				react: path.resolve(node_modules, 'react'),
+				'react-native': path.resolve(node_modules, 'react-native-web'),
+				'react-native-web': path.resolve(
+					node_modules,
+					'react-native-web'
+				),
+				'@expo/vector-icons': path.resolve(
+					node_modules,
+					'@expo/vector-icons'
+				),
+			})
+
+			fs.readdirSync(packages)
+				.filter((name) => !name.startsWith('.'))
+				.forEach((name) => {
+					config.resolve.alias[
+						name === 'dripsy' ? 'dripsy' : `@dripsy/${name}`
+					] = path.resolve(
+						packages,
+						name,
+						require(`../../packages/${name}/package.json`).source
+					)
+				})
+
+			return config
+		},
+	}),
+])
