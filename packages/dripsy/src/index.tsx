1e14f8c07af136ab71681eecc5e4a17a0eb2a209
Chadwick Maycumber <chad.maycumber11@gmail.com>
2021-02-11 14:50:56 -0500

    Broke out packages into @dripsy/core and dripsy

--- packages/dripsy/src/index.tsx
+++ packages/dripsy/src/index.tsx
@@ -1,13 +1,1 @@
-export * from './components'
-export * from './css/create-themed-component'
-export * from './provider'
-
-export { Styles, css, useResponsiveValue, useBreakpointIndex } from './css'
-// export { ThemeProvider, Th } from '@theme-ui/core'
-// export { InitializeColorMode } from 'theme-ui'
-export { DripsyProvider } from './provider'
-export type { Theme } from '@theme-ui/css'
-export { styled } from './css/styled'
-
-export { useThemeUI as useDripsyTheme } from '@theme-ui/core'
-export { remToPixels } from './utils/rem-to-pts'
+export * from '@dripsy/core'
