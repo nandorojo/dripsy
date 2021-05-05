efa4a82d60e8d9c240cc4a4d11856ab8f212afe2
Harris Robin Kalash <hello@harris.dev>
2021-05-05 16:02:25 -0400

    fix: add jsx pragma for classic runTime to ssr-component file

--- packages/core/src/css/ssr-component.tsx
+++ packages/core/src/css/ssr-component.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxRuntime classic */
 import { jsx, SxProps } from '@theme-ui/core'
 import { ThemeUIStyleObject } from '@theme-ui/css'
 import React, { ComponentProps, ComponentType, Fragment } from 'react'
