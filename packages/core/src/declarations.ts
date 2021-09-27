1ab5955e7e6e9e222d4fd70bf5bb95754ef3b141
Fernando Rojo <frojo@sas.upenn.edu>
2021-09-27 17:32:53 -0400

    add shadows to theme

--- packages/core/src/declarations.ts
+++ packages/core/src/declarations.ts
@@ -1,10 +1,30 @@
+import type { ViewStyle, TextStyle } from 'react-native'
 import type { Theme } from '@theme-ui/css'
-export interface DripsyBaseTheme extends Omit<Theme, 'fonts'> {
+export interface DripsyBaseTheme extends Omit<Theme, 'fonts' | 'shadows'> {
   customFonts?: { [key: string | number]: Record<string, string> }
   linearGradients?: {
     [key: string]: string[]
   }
   fonts?: Partial<Record<'root', string>> & Partial<Record<string, string>>
+  shadows?: {
+    [key: string]:
+      | Pick<
+          ViewStyle,
+          | 'elevation'
+          | 'shadowColor'
+          | 'shadowOffset'
+          | 'shadowOpacity'
+          | 'shadowRadius'
+        >
+      | Pick<
+          TextStyle,
+          'textShadowColor' | 'textShadowOffset' | 'textShadowRadius'
+        >
+  }
+}
+
+export function makeTheme<T extends DripsyBaseTheme>(theme: T): T {
+  return theme
 }
 
 export interface DripsyCustomTheme {
