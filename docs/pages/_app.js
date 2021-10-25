a8238999818fdff064f976e0aeb5d9316bbb0108
Fernando Rojo <frojo@sas.upenn.edu>
2021-10-25 18:30:05 -0400

    docs

---
+++ docs/pages/_app.js
@@ -0,0 +1,9 @@
+import 'nextra-theme-docs/style.css'
+
+import Prism from 'prism-react-renderer/prism'
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+require('prismjs/components/prism-bash.min')
+
+export default function Nextra({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ docs/theme.config.js
@@ -0,0 +1,36 @@
+// theme.config.js
+export default {
+  projectLink: 'https://github.com/nandorojo/dripsy', // GitHub link in the navbar
+  docsRepositoryBase: 'https://github.com/nandorojo/dripsy/blob/master', // base URL for the docs repository
+  titleSuffix: ' – Dripsy',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: `MIT ${new Date().getFullYear()} © Fernando Rojo.`,
+  footerEditLink: `Edit this page on GitHub ⚡️`,
+  logo: (
+    <>
+      <strong style={{ marginRight: 4 }}>Dripsy 🍷</strong>
+      <span>
+        by{' '}
+        <a target="_blank" rel="noreferrer" href="https://fernandorojo.co">
+          Fernando Rojo
+        </a>
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="Unstyled UI primitives for React Native (+ Web)"
+      />
+      <meta name="og:title" content="Dripsy by Fernando Rojo" />
+    </>
+  ),
+  unstable_faviconGlyph: '🍷',
+}
