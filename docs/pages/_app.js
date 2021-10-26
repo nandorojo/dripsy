a188382e397e073d7f7e7d686ca8b8b1790b38fa
Fernando Rojo <frojo@sas.upenn.edu>
2021-10-25 22:38:51 -0400

    analytics

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -4,6 +4,11 @@ import Prism from 'prism-react-renderer/prism'
 ;(typeof global !== 'undefined' ? global : window).Prism = Prism
 require('prismjs/components/prism-bash.min')
 require('prismjs/components/prism-diff.min')
+import splitbee from '@splitbee/web'
+
+splitbee.init({
+  token: '77UUK3L5TK3F',
+})
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
