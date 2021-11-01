862617213ed09c0ed2f0644ec02d459977a328b3
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-01 16:28:12 -0400

    Add contentContainerSx & indicatorSx props to `ScrollView`

--- packages/core/src/components/ScrollView.ts
+++
@@ -1,4 +0,0 @@
-import { ScrollView as rScrollView } from 'react-native'
-import { createThemedComponent } from '../css/create-themed-component'
-
-export const ScrollView = createThemedComponent(rScrollView)
---
+++ packages/core/src/components/ScrollView.tsx
@@ -0,0 +1,39 @@
+import React, { forwardRef } from 'react'
+import { ScrollView as rScrollView } from 'react-native'
+import { SxProp } from '../css/types'
+import { createThemedComponent } from '../css/create-themed-component'
+import { useSx } from '../use-sx'
+
+const DripsyScrollView = createThemedComponent(rScrollView)
+
+export type DripsyScrollViewProps = React.ComponentPropsWithoutRef<
+  typeof DripsyScrollView
+> & {
+  contentContainerSx?: SxProp
+  indicatorSx?: SxProp
+}
+
+export const ScrollView = forwardRef<rScrollView, DripsyScrollViewProps>(
+  function ScrollView(props, ref) {
+    const sx = useSx()
+    const containerSx = props.contentContainerSx && sx(props.contentContainerSx)
+    const indicatorSx = props.indicatorSx && sx(props.indicatorSx)
+
+    return (
+      <DripsyScrollView
+        {...props}
+        contentContainerStyle={
+          props.contentContainerStyle && containerSx
+            ? [props.contentContainerStyle, containerSx]
+            : containerSx || props.contentContainerStyle
+        }
+        indicatorStyle={
+          props.indicatorStyle && indicatorSx
+            ? [props.indicatorStyle, indicatorSx]
+            : indicatorSx || props.indicatorStyle
+        }
+        ref={ref}
+      />
+    )
+  }
+)
