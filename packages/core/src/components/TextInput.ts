dc4ec03c6239c1a9efe1e135378b0c424a3b9ff8
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-01 16:20:58 -0400

    feat: Themify selectionColor, placeholderTextColor, & underlineColorAndroid on TextInput

--- packages/core/src/components/TextInput.ts
+++
@@ -1,9 +0,0 @@
-import { TextInput as rTextInput } from 'react-native'
-import { createThemedComponent } from '../css/create-themed-component'
-import { defaultFontStyle } from './defaultStyle'
-
-export const TextInput = createThemedComponent(rTextInput, {
-  themeKey: 'forms',
-  defaultVariant: 'input',
-  defaultStyle: defaultFontStyle,
-})
---
+++ packages/core/src/components/TextInput.tsx
@@ -0,0 +1,45 @@
+import React, { forwardRef } from 'react'
+import { TextInput as rTextInput } from 'react-native'
+import { DripsyFinalTheme } from '../declarations'
+import { createThemedComponent } from '../css/create-themed-component'
+import { defaultFontStyle } from './defaultStyle'
+import { useDripsyTheme } from '../use-dripsy-theme'
+
+const DripsyInput = createThemedComponent(rTextInput, {
+  themeKey: 'forms',
+  defaultVariant: 'input',
+  defaultStyle: defaultFontStyle,
+})
+
+type InputProps = React.ComponentPropsWithoutRef<typeof DripsyInput>
+type ColorKeys = keyof Pick<
+  InputProps,
+  'selectionColor' | 'underlineColorAndroid' | 'placeholderTextColor'
+>
+
+export type DripsyTextInputProps = Omit<InputProps, ColorKeys> &
+  {
+    [key in ColorKeys]?:
+      | (string & {})
+      | Exclude<DripsyFinalTheme['colors'], object>
+  }
+
+const colorKeys: Record<ColorKeys, true> = {
+  selectionColor: true,
+  underlineColorAndroid: true,
+  placeholderTextColor: true,
+}
+
+export const TextInput = forwardRef<rTextInput, DripsyTextInputProps>(
+  function TextInput({ ...props }, ref) {
+    const { theme } = useDripsyTheme()
+    if (theme?.colors) {
+      Object.keys(colorKeys).forEach((key) => {
+        if (props[key] && theme.colors && key in theme.colors) {
+          props[key] = theme.colors[props[key]]
+        }
+      })
+    }
+    return <DripsyInput {...props} ref={ref} />
+  }
+)
