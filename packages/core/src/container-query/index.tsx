fc8a659f8f1c3c1b62971bb1e92d319ea671f3d5
Fernando Rojo <frojo@sas.upenn.edu>
2021-11-05 12:47:31 -0400

    feat: add fallbackStategy to <ContainerQuery />

--- packages/core/src/container-query/index.tsx
+++ packages/core/src/container-query/index.tsx
@@ -1,5 +1,6 @@
 import React, { ComponentProps, ReactNode, useCallback, useState } from 'react'
 import type { LayoutChangeEvent } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { getBreakpointIndex } from '../css/get-breakpoint-index'
 import { useBreakpoints } from '../css/breakpoints'
 import { View } from '../components'
@@ -13,6 +14,20 @@ type Props = {
    * then you should set this prop to that width. That way, there will be no empty layout state.
    */
   initialWidth?: number
+  /**
+   * Customize the strategy that should be used before the container query has determined its breakpoint.
+   *
+   * By default, the container will render the `fallback` prop (or null) until it has determined the container's width.
+   *
+   * However, you might not want this; it's possible that you still want to render the children, but just hide them.
+   *
+   * If this is the case, you can set this prop to `opacity`. In that case, children will still render, but they will just be visually hidden.
+   *
+   * This may be desired if you want to run network requests and other effects as children of this component, without waiting for the width to get determined.
+   *
+   * Default: `render`.
+   */
+  fallbackStrategy?: 'render' | 'opacity'
 } & Pick<ComponentProps<typeof View>, 'sx' | 'pointerEvents'>
 
 export function ContainerQuery({
@@ -21,6 +36,7 @@ export function ContainerQuery({
   initialWidth,
   sx,
   pointerEvents,
+  fallbackStrategy,
 }: Props) {
   const breakpoints = useBreakpoints()
 
@@ -45,11 +61,34 @@ export function ContainerQuery({
     [breakpoints]
   )
 
+  let child: ReactNode = (
+    <BreakpointIndexContext.Provider value={breakpointIndex}>
+      {breakpointIndex !== -1 ? children : fallback}
+    </BreakpointIndexContext.Provider>
+  )
+
+  if (fallbackStrategy == 'opacity' && breakpointIndex !== -1) {
+    child = children
+  }
+
   return (
-    <View onLayout={onLayout} sx={sx} pointerEvents={pointerEvents}>
-      <BreakpointIndexContext.Provider value={breakpointIndex}>
-        {breakpointIndex !== -1 ? children : fallback}
-      </BreakpointIndexContext.Provider>
+    <View
+      style={
+        fallbackStrategy == 'opacity' && breakpointIndex === -1
+          ? styles.visuallyHidden
+          : undefined
+      }
+      onLayout={onLayout}
+      sx={sx}
+      pointerEvents={pointerEvents}
+    >
+      {child}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  visuallyHidden: {
+    opacity: 0,
+  },
+})
