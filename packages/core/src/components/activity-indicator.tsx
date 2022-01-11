59d72f2a5abf6c20e9adb67b7c65b58bf3c3df4e
Fernando Rojo <frojo@sas.upenn.edu>
2022-01-11 15:16:15 -0500

    feat: activity indicator color prop intellisense

--- packages/core/src/components/activity-indicator.tsx
+++ packages/core/src/components/activity-indicator.tsx
@@ -1,21 +1,29 @@
 import React, { ComponentProps } from 'react'
 import { ActivityIndicator as NativeActivityIndicator } from 'react-native'
 import { useDripsyTheme } from '../use-dripsy-theme'
-import { createThemedComponent } from '../css/create-themed-component'
 import { DripsyFinalTheme } from '../declarations'
+import { useSx } from '../use-sx'
+import { SxProp } from '../css/types'
 
 type Props = Omit<ComponentProps<typeof NativeActivityIndicator>, 'color'> & {
   color?: (string & {}) | keyof DripsyFinalTheme['colors']
 }
 
-function Indicator(props: Props) {
+function Indicator(props: Props & { sx?: SxProp }) {
   const { colors } = useDripsyTheme().theme
+  const sx = useSx()
 
   let { color } = props
   if (typeof color === 'string' && colors?.[color]) {
     color = colors[color] as string
   }
-  return <NativeActivityIndicator {...props} color={color} />
+  return (
+    <NativeActivityIndicator
+      {...props}
+      color={color as string}
+      style={[props.style, props.sx && sx(props.sx)]}
+    />
+  )
 }
 
-export const ActivityIndicator = createThemedComponent(Indicator)
+export const ActivityIndicator = Indicator
