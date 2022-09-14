9f6e35903a4b190095f940a8a91654b9161303ee
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2022-09-14 15:03:30 +0100

    extra props should take precedence

--- packages/core/src/css/create-themed-component.tsx
+++ packages/core/src/css/create-themed-component.tsx
@@ -6,13 +6,17 @@ import { mapPropsToStyledComponent } from './map-props'
 import { DripsyFinalTheme } from '../declarations'
 import { useStableMemo } from '../utils/use-stable-memo'
 
+type MergeProps<P1, P2> = Omit<P1, keyof P2> & P2
+type PropsWithoutVariants<P> = Omit<P, 'variant' | 'variants'>
+type ExtendedProps<C, ExtraProps> = C extends ComponentType<infer BaseProps>
+  ? MergeProps<PropsWithoutVariants<BaseProps>, ExtraProps>
+  : never
+
 export function createThemedComponent<
   C extends ComponentType<any>,
   ExtraProps,
   ThemeKey extends keyof DripsyFinalTheme,
-  BaseComponentProps extends { style?: any } = C extends ComponentType<infer P>
-    ? Omit<P, 'variant' | 'variants'>
-    : never
+  Props extends { style?: any } = ExtendedProps<C, ExtraProps>
 >(
   Component: C,
   {
@@ -21,60 +25,59 @@ export function createThemedComponent<
   }: ThemedOptions<ExtraProps, ThemeKey> = {}
 ): React.ForwardRefExoticComponent<
   StyledProps<ThemeKey> &
-    React.PropsWithoutRef<PropsWithChildren<BaseComponentProps & ExtraProps>> &
+    React.PropsWithoutRef<PropsWithChildren<Props>> &
     React.RefAttributes<React.ElementRef<C>>
 > {
-  const WrappedComponent = React.forwardRef<
-    any,
-    StyledProps<ThemeKey> & BaseComponentProps & ExtraProps
-  >(function Wrapped(prop, ref) {
-    const {
-      sx: _sx,
-      as: SuperComponent,
-      variant,
-      style,
-      themeKey = options.themeKey,
-      variants = options.defaultVariants,
-      ...props
-    } = prop
-    if (typeof __DEV__ !== 'undefined' && typeof SuperComponent === 'string') {
-      console.error(
-        `[dripsy] Looks like you used an invalid "as" prop. "${SuperComponent}" can't be string. Please pass a valid React component. HTML elements are not supported.`
+  const WrappedComponent = React.forwardRef<any, StyledProps<ThemeKey> & Props>(
+    function Wrapped(prop, ref) {
+      const {
+        sx: _sx,
+        as: SuperComponent,
+        variant,
+        style,
+        themeKey = options.themeKey,
+        variants = options.defaultVariants,
+        ...props
+      } = prop
+      if (typeof __DEV__ !== 'undefined' && typeof SuperComponent === 'string') {
+        console.error(
+          `[dripsy] Looks like you used an invalid "as" prop. "${SuperComponent}" can't be string. Please pass a valid React component. HTML elements are not supported.`
+        )
+      }
+      const defaultStyle =
+        typeof baseStyle == 'function' ? baseStyle(prop) : baseStyle
+
+      const { theme } = useDripsyTheme()
+      // make the sx factory out here so that it's a stable dependency for useStableMemo
+      const sx = typeof _sx == 'function' ? _sx(theme) : _sx
+
+      const breakpoint = useBreakpointIndex()
+
+      const { styles } = useStableMemo(
+        () =>
+          mapPropsToStyledComponent<ThemeKey>(
+            {
+              theme,
+              breakpoint,
+              variant,
+              sx,
+              style,
+              variants: variants as DripsyVariant<ThemeKey>[] | undefined,
+            },
+            {
+              ...options,
+              themeKey,
+              defaultStyle,
+            }
+          ),
+        [theme, breakpoint, variant, sx, style, variants, themeKey, defaultStyle]
       )
+
+      const TheComponent = SuperComponent || Component
+
+      return <TheComponent {...(props as any)} ref={ref} style={styles} />
     }
-    const defaultStyle =
-      typeof baseStyle == 'function' ? baseStyle(prop) : baseStyle
-
-    const { theme } = useDripsyTheme()
-    // make the sx factory out here so that it's a stable dependency for useStableMemo
-    const sx = typeof _sx == 'function' ? _sx(theme) : _sx
-
-    const breakpoint = useBreakpointIndex()
-
-    const { styles } = useStableMemo(
-      () =>
-        mapPropsToStyledComponent<ThemeKey>(
-          {
-            theme,
-            breakpoint,
-            variant,
-            sx,
-            style,
-            variants: variants as DripsyVariant<ThemeKey>[] | undefined,
-          },
-          {
-            ...options,
-            themeKey,
-            defaultStyle,
-          }
-        ),
-      [theme, breakpoint, variant, sx, style, variants, themeKey, defaultStyle]
-    )
-
-    const TheComponent = SuperComponent || Component
-
-    return <TheComponent {...(props as any)} ref={ref} style={styles} />
-  })
+  )
 
   WrappedComponent.displayName = `Dripsy.${
     Component?.displayName ?? 'NoNameComponent'
